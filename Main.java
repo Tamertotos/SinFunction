f91efa6daed226c057d90a786a3eccf0d7c7e33f
Tamertotos <tamercantotos@gmail.com>
2026-03-15 09:29:02 +0100

    Refactor taylor series SIN calculator with range reduction and modulo optimization

--- Main.java
+++ Main.java
@@ -1,16 +1,50 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        int value = 8;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a degree for its sine value");
 
-        int test = 5;
-        System.out.println("factorial value of " + test + " is equal to: " +  factorial(test));
+        int value;
+
+        while (true){
+            try {
+                value = scanner.nextInt();
+                break;
+            } catch (InputMismatchException ex){
+                System.out.println("Please enter a number");
+                scanner.next();
+            }
+        }
+
+
+
+        while (value < 0){
+            value += 360;
+        }
+
+        if (value > 360){
+            value %= 360;
+        }
+
 
         System.out.println("radians value of degree " + value + " is: " + toRadians(value));
         System.out.println("Sin(" + value + ") is equal to " + sinValue(value));
     }
 
+
     public static double sinValue (int x){
-        return  toRadians(x) - (Math.pow(toRadians(x),3)/factorial(3))+(Math.pow(toRadians(x),5)/factorial(5));
+        double limitedRadians = limitedRadiansValue(toRadians(x));
+
+        return  limitedRadians - (Math.pow(limitedRadians,3)/factorial(3))
+                +(Math.pow(limitedRadians,5)/factorial(5))
+                -(Math.pow(limitedRadians,7)/factorial(7))
+                +(Math.pow(limitedRadians,9)/factorial(9));
+    }
+
+    public static double limitedRadiansValue(double x){
+        return Math.PI - x;
     }
 
     public static double toRadians (int a){
